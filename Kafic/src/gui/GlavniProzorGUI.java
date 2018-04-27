6ecd9c5fadcf30898eae524e8ebb7887fc228f52
sreckoN <srecko.nikolic71@hotmail.com>
2018-04-27 23:35:36 +0200

    Promenjen naslov glavnog prozora

--- Kafic/src/gui/GlavniProzorGUI.java
+++ Kafic/src/gui/GlavniProzorGUI.java
@@ -6,10 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class GlavniProzorGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnRacun;
 
 	/**
 	 * Launch the application.
@@ -31,12 +33,20 @@ public class GlavniProzorGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GlavniProzorGUI() {
+		setTitle("Kafic");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.add(getBtnRacun(), BorderLayout.CENTER);
 	}
 
+	private JButton getBtnRacun() {
+		if (btnRacun == null) {
+			btnRacun = new JButton("Racun");
+		}
+		return btnRacun;
+	}
 }
